7b96a82a90d692b5f25fe030a037a6dff8f910fa
Dan Brown <ssddanbrown@googlemail.com>
2022-07-15 17:05:47 +0100

    Preserved whitespace in notes

--- resources/views/notes/parts/note-card.blade.php
+++ resources/views/notes/parts/note-card.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-3">
     <div class="card-body">
-        <p class="mb-1">{{ $note->text }}</p>
+        <p class="mb-1 show-whitespace">{{ $note->text }}</p>
         <div class="small text-muted">
             <i>Written {{ $note->created_at->diffForHumans() }}</i> |
             <a href="{{ route('note.edit', compact('note')) }}">Edit</a>
