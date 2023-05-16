a295eb961b7fbc170307cd27e34f7dda70d85dd6
Shift <shift@laravelshift.com>
2023-05-16 10:57:18 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/services.php
+++ config/services.php
@@ -2,6 +2,12 @@
 
 return [
 
+    'hcaptcha' => [
+        'active' => ! empty(env('HCAPTCHA_SITEKEY', null)),
+        'sitekey' => env('HCAPTCHA_SITEKEY', null),
+        'secretkey' => env('HCAPTCHA_SECRETKEY', null),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -14,16 +20,11 @@ return [
     |
     */
 
-    'hcaptcha' => [
-        'active' => ! empty(env('HCAPTCHA_SITEKEY', null)),
-        'sitekey' => env('HCAPTCHA_SITEKEY', null),
-        'secretkey' => env('HCAPTCHA_SECRETKEY', null),
-    ],
-
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
     ],
 
     'postmark' => [
